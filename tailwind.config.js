a9830860c4c72b27869e61e89964e4a0c13be1be
Christian <Christian.m.panerio@gmail.com>
2025-08-13 13:21:21 +0800

    04: homescreen ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#001F4D",      // Dark navy blue
-        secondary: "#003366",    // Medium dark blue
+        primary: "#001F4D",      // Dark navy blue // #030014
+        secondary: "#003366",    // Medium dark blue // #151312
         light: {
-          100: "#99CCFF",        // Light sky blue
-          200: "#66B2FF",        // Soft blue
-          300: "#3399FF",        // Bright blue
+          100: "#99CCFF",        // Light sky blue // #D6C7FF
+          200: "#66B2FF",        // Soft blue // #A8B5DB
+          300: "#3399FF",        // Bright blue // #9CA4AB
         },
         dark: {
-          100: "#00264D",        // Darker navy
-          200: "#001933",        // Very dark blue
+          100: "#00264D",        // Darker navy // #221F3D
+          200: "#001933",        // Very dark blue // #0F0D23
         },
-        accent: "#4DA6FF",       // Bright accent blue
+        accent: "#4DA6FF",       // Bright accent blue // AB8BFF
       },
 
     },
